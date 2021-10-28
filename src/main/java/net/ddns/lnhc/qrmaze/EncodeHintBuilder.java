1188d6c3de7d91ead2c36cee5a7c6557819202c0
mrbrique <locus.chan.dev@gmail.com>
2021-10-29 01:48:32 +0800

    Create EncodeHintBuilder.java
    add EncodeHintBuilder

---
+++ src/main/java/net/ddns/lnhc/qrmaze/EncodeHintBuilder.java
@@ -0,0 +1,64 @@
+package net.ddns.lnhc.qrmaze;
+
+import java.util.HashMap;
+
+import com.google.zxing.EncodeHintType;
+
+public class EncodeHintBuilder {
+
+	private HashMap<EncodeHintType, Object> hintMap;
+
+	public EncodeHintBuilder() {
+		hintMap = new HashMap<EncodeHintType, Object>();
+	}
+
+	public EncodeHintBuilder setAztecLayers(Object o) {
+		hintMap.put(EncodeHintType.AZTEC_LAYERS, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setCharacterSet(Object o) {
+		hintMap.put(EncodeHintType.CHARACTER_SET, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setDataMatrixShape(Object o) {
+		hintMap.put(EncodeHintType.DATA_MATRIX_SHAPE, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setErrorCorrection(Object o) {
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setMargin(Object o) {
+		hintMap.put(EncodeHintType.MARGIN, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setQrVersion(Object o) {
+		hintMap.put(EncodeHintType.QR_VERSION, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setPdf417Compact(Object o) {
+		hintMap.put(EncodeHintType.PDF417_COMPACTION, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setPdf417Compaction(Object o) {
+		hintMap.put(EncodeHintType.PDF417_COMPACTION, o);
+		return this;
+	}
+
+	public EncodeHintBuilder setPdf417Dimensions(Object o) {
+		hintMap.put(EncodeHintType.PDF417_DIMENSIONS, o);
+		return this;
+	}
+
+	public HashMap<EncodeHintType, Object> build() {
+		return hintMap;
+	}
+
+}
