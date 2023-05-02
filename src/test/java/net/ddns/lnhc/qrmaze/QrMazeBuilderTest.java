cae9851b9afffdc983538cd07c31a48172758de5
mrbrique <locus.chan.dev@gmail.com>
2023-05-02 02:27:12 +0100

    update test case

--- src/test/java/net/ddns/lnhc/qrmaze/QrMazeBuilderTest.java
+++ src/test/java/net/ddns/lnhc/qrmaze/QrMazeBuilderTest.java
@@ -9,7 +9,11 @@ public class QrMazeBuilderTest {
 		String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 		QrMazeBuilder.main(new String[] { lorem });
 
-		QrMazeBuilder.main(new String[] { "-v", "6", "hello world" });
+		QrMazeBuilder.main(new String[] { "-v", "7", lorem });
+
+		QrMazeBuilder.main(new String[] { "-c", "H", lorem });
+
+		QrMazeBuilder.main(new String[] { "-v", "12", "-c", "H", lorem });
 	}
 
 }
